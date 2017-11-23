a58dee031a0e7d3c46b319ff113f4676d8950a55
juan <juan@domatix.com>
2017-11-23 10:55:08 +0100

    Readme y vistas

---
+++ dx_iva_record/README.rst
@@ -0,0 +1,37 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+    :alt: License: AGPL-3
+
+================================
+Libro de IVA
+================================
+
+Informe PDF del libro de IVA. Genera un PDF con los impuestos de las facturas entre las fechas seleccionadas, se agrupa por impuesto.
+Genera un documento para las bases negativas y otro para las positivas y se separa el IGIC en documentos aparte.
+
+Uso
+===
+
+Para generar el informe:
+
+1. Ir a Contabilidad > Informes PDF > Registro de IVA
+2. Seleccionar facturas de cliente o proveedor. En blanco saca los dos tipos.
+3. Seleccionar compañia, por defecto compañia actual.
+4. Seleccionar fechas.
+5. Seleccionar impuestos, al seleccionar el tipo de factura se rellenara con los impuestos de venta o compra.
+6. Imprimir mediante el boton 'Imprimir registro de IVA'
+
+Créditos
+========
+
+Contribuidores
+--------------
+
+* Juan Cuesta Contreras <juan@domatix.com>
+* Nacho Serra Almenar <nacho.serra@domatix.com>
+
+Maintainer
+----------
+ * Domatix  <https://github.com/Domatix>
+
+To contribute to this module, please visit https://github.com/Domatix/accounting
--- dx_iva_record/wizard/iva_record.py
+++ dx_iva_record/wizard/iva_record.py
@@ -1,24 +1,7 @@
 # -*- coding: utf-8 -*-
-###############################################################################
-#    Module created by domatix
-#    OpenERP, Open Source Management Solution
-#
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from odoo import models, fields
+# Copyright 2017 Domatix - Juan Cuesta Contreras
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import models, fields, api
 from itertools import groupby
 
 
@@ -27,14 +10,14 @@ class iva_record_wizzard(models.TransientModel):
 
     def get_data(self):
         if self.partner_type == 'supplier':
-            invoice_tax_obj = self.env['account.invoice.tax'].search([('invoice_id.date', '>=', self.date_start), ('invoice_id.date', '<=', self.date_end), ('company_id', '=', self.company_id.id), ('invoice_id.state', 'in', ['open', 'paid']), ('invoice_id.type', 'in', ['in_invoice', 'in_refund'])])
+            invoice_tax_obj = self.env['account.invoice.tax'].search([('invoice_id.date', '>=', self.date_start), ('invoice_id.date', '<=', self.date_end), ('tax_id.id', 'in', self.tax_ids.ids), ('company_id', '=', self.company_id.id), ('invoice_id.state', 'in', ['open', 'paid']), ('invoice_id.type', 'in', ['in_invoice', 'in_refund'])])
         elif self.partner_type == 'client':
-            invoice_tax_obj = self.env['account.invoice.tax'].search([('invoice_id.date', '>=', self.date_start), ('invoice_id.date', '<=', self.date_end), ('company_id', '=', self.company_id.id), ('invoice_id.state', 'in', ['open', 'paid']), ('invoice_id.type', 'in', ['out_invoice', 'out_refund'])])
+            invoice_tax_obj = self.env['account.invoice.tax'].search([('invoice_id.date', '>=', self.date_start), ('invoice_id.date', '<=', self.date_end), ('tax_id.id', 'in', self.tax_ids.ids), ('company_id', '=', self.company_id.id), ('invoice_id.state', 'in', ['open', 'paid']), ('invoice_id.type', 'in', ['out_invoice', 'out_refund'])])
         else:
-            invoice_tax_obj = self.env['account.invoice.tax'].search([('invoice_id.date', '>=', self.date_start), ('invoice_id.date', '<=', self.date_end)])
+            invoice_tax_obj = self.env['account.invoice.tax'].search([('invoice_id.date', '>=', self.date_start), ('tax_id.id', 'in', self.tax_ids.ids), ('invoice_id.date', '<=', self.date_end)])
 
-        positive = invoice_tax_obj.filtered(lambda r: r.invoice_id.amount_untaxed_signed > 0 and 'igic' not in r.name and 'IVA' in r.name)
-        negative = invoice_tax_obj.filtered(lambda r: r.invoice_id.amount_untaxed_signed < 0 and 'igic' not in r.name and 'IVA' in r.name)
+        negative = invoice_tax_obj.filtered(lambda r: r.invoice_id.amount_untaxed_signed < 0 and 'IGIC' not in r.name)
+        positive = invoice_tax_obj.filtered(lambda r: r.invoice_id.amount_untaxed_signed > 0 and 'IGIC' not in r.name)
 
         res_dict = {}
         positive = sorted(positive, key=lambda x: x.invoice_id.move_id.date)
@@ -52,25 +35,36 @@ class iva_record_wizzard(models.TransientModel):
                 res_dict['negative'][key] = []
             for tax in group:
                 res_dict['negative'][key].append(tax)
-        if self.igic:
-            igic_negative = invoice_tax_obj.filtered(lambda r: r.invoice_id.amount_untaxed_signed < 0 and 'igic' in r.name and 'IVA' not in r.name)
-            igic_positive = invoice_tax_obj.filtered(lambda r: r.invoice_id.amount_untaxed_signed > 0 and 'igic' in r.name and 'IVA' not in r.name)
-            igic_positive = sorted(igic_positive, key=lambda x: x.invoice_id.move_id.date)
-            igic_negative = sorted(igic_negative, key=lambda x: x.invoice_id.move_id.date)
-            res_dict['igic_positive'] = {}
-            res_dict['igic_negative'] = {}
-            for key, group in groupby(igic_positive, lambda x: x.name):
-                if key not in res_dict['igic_positive']:
-                    res_dict['igic_positive'][key] = []
-                for tax in group:
-                    res_dict['igic_positive'][key].append(tax)
-            for key, group in groupby(igic_negative, lambda x: x.name):
-                if key not in res_dict['igic_negative']:
-                    res_dict['igic_negative'][key] = []
-                for tax in group:
-                    res_dict['igic_negative'][key].append(tax)
+
+        igic_negative = invoice_tax_obj.filtered(lambda r: r.invoice_id.amount_untaxed_signed < 0 and 'IGIC' in r.name)
+        igic_positive = invoice_tax_obj.filtered(lambda r: r.invoice_id.amount_untaxed_signed > 0 and 'IGIC' in r.name)
+        igic_positive = sorted(igic_positive, key=lambda x: x.invoice_id.move_id.date)
+        igic_negative = sorted(igic_negative, key=lambda x: x.invoice_id.move_id.date)
+        res_dict['igic_positive'] = {}
+        res_dict['igic_negative'] = {}
+        for key, group in groupby(igic_positive, lambda x: x.name):
+            if key not in res_dict['igic_positive']:
+                res_dict['igic_positive'][key] = []
+            for tax in group:
+                res_dict['igic_positive'][key].append(tax)
+        for key, group in groupby(igic_negative, lambda x: x.name):
+            if key not in res_dict['igic_negative']:
+                res_dict['igic_negative'][key] = []
+            for tax in group:
+                res_dict['igic_negative'][key].append(tax)
         return res_dict.items()
 
+    @api.onchange("partner_type")
+    def _onchange_field(self):
+        if self.partner_type == 'client':
+            self.tax_ids = self.env['account.tax'].search([('type_tax_use',
+                                                            '=', 'sale')]).ids
+        elif self.partner_type == 'supplier':
+            self.tax_ids = self.env['account.tax'].search([('type_tax_use',
+                                                            '=',
+                                                            'purchase')]).ids
+
+    tax_ids = fields.Many2many(comodel_name='account.tax', string='Taxes')
     company_id = fields.Many2one('res.company', 'Company',
                                  help='Company',
                                  required=True,
